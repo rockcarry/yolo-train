6ca5b5ebe47c7a75ec5ee1a7238200bb79aaefeb
ck <rockcarry@163.com>
2021-07-28 22:24:48 +0800

    update voc_label.py

--- voc_label.py
+++ voc_label.py
@@ -6,8 +6,8 @@ from os.path import join
 
 sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
-classes = ["person", "cat", "dog"]
-
+classes1 = ["person", "cat", "dog"]
+classes2 = ["bottle", "cat", "chair", "diningtable", "dog", "person", "pottedplant", "sofa", "tvmonitor" ]
 
 def convert(size, box):
     dw = 1./(size[0])
@@ -30,17 +30,21 @@ def convert_annotation(year, image_id):
     size = root.find('size')
     w = int(size.find('width').text)
     h = int(size.find('height').text)
+    f = False
 
     for obj in root.iter('object'):
         difficult = obj.find('difficult').text
         cls = obj.find('name').text
-        if cls not in classes or int(difficult)==1:
+        if cls not in classes1 or int(difficult)==1:
             continue
-        cls_id = classes.index(cls)
+        if cls in classes2:
+            f = True
+        cls_id = classes1.index(cls)
         xmlbox = obj.find('bndbox')
         b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
         bb = convert((w,h), b)
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+    return f
 
 wd = getcwd()
 
@@ -50,8 +54,8 @@ for year, image_set in sets:
     image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
     list_file = open('%s_%s.txt'%(year, image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
-        convert_annotation(year, image_id)
+        if convert_annotation(year, image_id) == True:
+            list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
     list_file.close()
 
 os.system("cat 2007_train.txt 2007_val.txt 2012_train.txt 2012_val.txt > train.txt")
